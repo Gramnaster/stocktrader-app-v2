85dd7f5aa708cea572c74b842f30d0c32f799adf
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2025-09-22 20:33:17 +0800

    Uncomment user model validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,42 @@
  class User < ApplicationRecord
-#   # Include default devise modules. Others available are:
-#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#   include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
 
-#   devise :database_authenticatable, :registerable,
-#          :recoverable, :rememberable, :validatable,
-#          :confirmable, :timeoutable,
-#          :jwt_authenticatable, jwt_revocation_strategy: self
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :timeoutable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
-#   enum :user_status, { pending: "pending", approved: "approved", rejected: "rejected" }
-#   enum :user_role, { trader: "trader", admin: "admin" }
+  enum :user_status, { pending: "pending", approved: "approved", rejected: "rejected" }
+  enum :user_role, { trader: "trader", admin: "admin" }
 
-#   # Automatically create a wallet when a new user is created
-#   after_create :create_user_wallet
+  # Automatically create a wallet when a new user is created
+  after_create :create_user_wallet
 
-#   validates :first_name, presence: true
-#   validates :last_name, presence: true
-#   validates :date_of_birth, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
 
-#   validates :mobile_no, presence: true, uniqueness: true
-#   validates :zip_code, presence: true
+  validates :mobile_no, presence: true, uniqueness: true
+  validates :zip_code, presence: true
 
-#   belongs_to :country
-#   validates :country, presence: true
+  belongs_to :country
+  validates :country, presence: true
 
-#   has_one :wallet, dependent: :destroy
+  has_one :wallet, dependent: :destroy
 
-#   has_many :portfolios, dependent: :destroy
-#   has_many :stocks, through: :portfolios
-#   has_many :transactions, dependent: :destroy
-#   has_many :stock_reviews, dependent: :destroy
+  has_many :portfolios, dependent: :destroy
+  has_many :stocks, through: :portfolios
+  has_many :transactions, dependent: :destroy
+  has_many :stock_reviews, dependent: :destroy
 
-#   private
+  private
 
-#   def create_user_wallet
-#     Wallet.create!(
-#       user: self,
-#       balance: 0.0  # Starting balance of $0
-#     )
-#   end
+  def create_user_wallet
+    Wallet.create!(
+      user: self,
+      balance: 0.0  # Starting balance of $0
+    )
+  end
  end
