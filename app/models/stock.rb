8c60ca5ead7e67082b72fe8c92728004b36a9491
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-23 22:28:35 +0800

    Feat: Receipts table basic setup is completed

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,8 @@ class Stock < ApplicationRecord
   has_many :portfolios, dependent: :destroy
   has_many :users, through: :portfolios
   has_many :historical_prices, dependent: :destroy
-  has_many :transactions, dependent: :destroy
+  # has_many :transactions, dependent: :destroy
+  has_many :receipts, dependent: :destroy
 
   validates :ticker, presence: true, uniqueness: true
   validates :name, presence: true
--- db/migrate/20250923140615_drop_transactions_table_and_enum.rb
+++ db/migrate/20250923140615_drop_transactions_table_and_enum.rb
@@ -1,25 +1,6 @@
-class CreateTransactions < ActiveRecord::Migration[8.0]
+class DropTransactionsTableAndEnum < ActiveRecord::Migration[8.0]
   def up
-    execute <<-SQL
-      CREATE TYPE IF NOT EXISTS transaction_type AS ENUM ('buy', 'sell');
-    SQL
-
-    create_table :transactions do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :stock, null: false, foreign_key: true
-
-      t.column :transaction_type, :transaction_type, null: false
-
-      t.decimal :quantity, precision: 15, scale: 5, null: false
-      t.decimal :price_per_share, precision: 15, scale: 2, null: false
-      t.decimal :total_amount, precision: 15, scale: 2, null: false
-
-      t.timestamps
-    end
-  end
-
-  def down
-    drop_table :transaction
+    drop_table :transactions, if_exists: true
 
     execute <<-SQL
       DO $$
@@ -30,5 +11,21 @@ class CreateTransactions < ActiveRecord::Migration[8.0]
       END
       $$;
     SQL
+  end
+
+  def down
+    execute <<-SQL
+      CREATE TYPE IF NOT EXISTS transaction_type AS ENUM ('buy', 'sell');
+    SQL
+
+    create_table :transactions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :stock, null: false, foreign_key: true
+      t.column :transaction_type, :transaction_type, null: false
+      t.decimal :quantity, precision: 15, scale: 5, null: false
+      t.decimal :price_per_share, precision: 15, scale: 2, null: false
+      t.decimal :total_amount, precision: 15, scale: 2, null: false
+      t.timestamps
+    end
   end
 end
