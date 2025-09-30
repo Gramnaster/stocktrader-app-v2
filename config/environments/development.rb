f8fb9b01a18f565fc7c9f704efd2bac902567ac5
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-30 11:43:05 +0800

    Email: Allowed the mailer to send to gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,27 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Configure Action Mailer for real email delivery via Gmail SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
+
+  # Gmail SMTP configuration
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV["GMAIL_USERNAME"],      # Set this in your environment
+    password: ENV["GMAIL_APP_PASSWORD"],   # Use Gmail App Password, not regular password
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
